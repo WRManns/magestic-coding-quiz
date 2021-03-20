dcb3fdcb330030c0d230c7f64dbc51acde949677
Ryan Manns <w.r.manns@outlook.com>
2021-03-19 20:58:36 -0400

    adding highscore html

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,11 +1,13 @@
 var timer = document.querySelector(".timer");
 var timeLeft = 100;
+timer.textContent = timeLeft;
 
 function setTime() {
     
     var timerInterval = setInterval(function() {
       timeLeft--;
       timer.textContent = timeLeft;
+      
   
       if(timeLeft === 0) {
         clearInterval(timerInterval);
