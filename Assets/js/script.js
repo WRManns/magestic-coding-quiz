0ba68c255cc337d8794b36840fa2d6f6725b8d4c
Ryan Manns <w.r.manns@outlook.com>
2021-03-23 17:44:32 -0400

    recommit timerEl fixed and highscore html linked

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,21 +1,56 @@
-var timer = document.querySelector(".timer");
-var timeLeft = 100;
-timer.textContent = timeLeft;
+var javaQuestions = [
+  {
+      question: "Commonly used data types DO NOT include:",
+      choices: ["strings", "booleans", "alerts", "numbers"],
+      answer: "alerts"
+  },
+  {
+      question: "The condition in an if / else statement is enclosed within ____.",
+      choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
+      answer: "parentheses"
+  },
+  {
+      question: "Arrays in Javascript can be used to store:",
+      choices: ["number and strings", "other arrays", "booleans", "all of the above"],
+      answer: "all of the above"
+  },
+  {
+      question: "String values must be enclosed within ____ when being assigned to variables",
+      choices: ["commas", "curly brackets", "quotes", "parantheses"],
+      answer: "quotes"
+  },
+  {
+      question: "A very useful tool used during development and debugging for printing content to the debugger is:",
+      choices: ["Javascript", "terminal/bash", "for loops", "console.log"],
+      answer: "console.log"
+  }
+];
 
-function setTime() {
-    
-    var timerInterval = setInterval(function() {
-      timeLeft--;
-      timer.textContent = timeLeft;
-      
-  
-      if(timeLeft === 0) {
-         // Clears interval and runs endGame function
-          clearInterval(timer);
-          endGame();
-        }
-        clearInterval(timerInterval);
-      }, 1000);  
+var timerEl = document.getElementById("time");
+var startBtn = document.getElementById("start-btn");
+var submitBtn = document.getElementById("submit-btn");
+var questionEl= document.getElementById("questions");
+var answersEl= document.getElementById("answers");
+
+var timer;
+var timerCount;
+
+function startQuiz() {
+  document.getElementById("quiz-home").style.display = "none";
+  document.getElementById("quiz-card").style.display = "block";
+  timerCount = 100;
+  startTimer()
 }
 
-setTime();
+function startTimer() {
+  timer = setInterval(function() {
+    timerCount--;
+    timerEl.textContent = timerCount;
+
+    if (timerCount === 0) {
+      clearInterval(timer);
+    }
+  }, 1000);
+}
+
+startBtn.addEventListener("click", startQuiz);
