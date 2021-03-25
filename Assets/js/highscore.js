2372727eb82611608ca4cc7ae5171d0ee67e781d
Ryan Manns <w.r.manns@outlook.com>
2021-03-24 20:31:37 -0400

    able to move through quiz and store-display-and clear scores and navigate back to quiz--styling needed

---
+++ Assets/js/highscore.js
@@ -0,0 +1,30 @@
+var clearBtn = document.getElementById("clear-button");
+var returnBtn = document. getElementById("return-button");
+var highScoreTable = document.getElementById("highscores");
+var highScores=[];
+
+highScores= JSON.parse(localStorage.getItem("highScores"));
+
+function renderHighscore() {
+    
+    if (localStorage && highScores !== null) {
+        for (let i=0; i < highScores.length; i++) {
+            var newHighscore = document.createElement("tr");
+            var newCell = document.createElement("td");
+            newCell.textContent = (i + 1) + "." + highScores[i].name + " " + highScores[i].score;
+            newHighscore.appendChild(newCell);
+            highScoreTable.appendChild(newHighscore);
+        }
+    }
+}
+renderHighscore();
+
+clearBtn.addEventListener("click", function() {
+    localStorage.clear();
+    highScores= [];
+    highScoreTable.innerHTML = "";
+})
+
+returnBtn.addEventListener('click', function() {
+    window.location.href= "./index.html"
+})
--- Assets/js/script.js
+++ Assets/js/script.js
@@ -23,6 +23,11 @@ var javaQuestions = [
       question: "A very useful tool used during development and debugging for printing content to the debugger is:",
       choices: ["Javascript", "terminal/bash", "for loops", "console.log"],
       answer: "console.log"
+  },
+  {
+      question: "",
+      choices: [],
+      answer: ""
   }
 ];
 
@@ -30,16 +35,23 @@ var timerEl = document.getElementById("time");
 var startBtn = document.getElementById("start-btn");
 var submitBtn = document.getElementById("submit-btn");
 var questionEl= document.getElementById("questions");
-var answersEl= document.getElementById("answers");
-
+var quizContainer= document.getElementById("quiz");
+var choiceContainer= document.getElementById("choices");
+var userScore= document.getElementById("end-score");
+var userInitials;
 var timer;
 var timerCount;
+var currentQuestion = 0;
+var questionNumber = 0;
+var answer;
+
 
 function startQuiz() {
   document.getElementById("quiz-home").style.display = "none";
   document.getElementById("quiz-card").style.display = "block";
   timerCount = 100;
   startTimer()
+  getQuestion()
 }
 
 function startTimer() {
@@ -47,10 +59,87 @@ function startTimer() {
     timerCount--;
     timerEl.textContent = timerCount;
 
-    if (timerCount === 0) {
+    if (timerCount === 0 || questionNumber === javaQuestions.length) {
       clearInterval(timer);
+      setTimeout(endGame, 500);       
     }
   }, 1000);
 }
 
+function getQuestion() {    
+
+    quizContainer.textContent = javaQuestions[questionNumber].question;
+    choiceContainer.innerHTML = "";
+
+    answer = javaQuestions[questionNumber].answer;
+
+    var choices = javaQuestions[questionNumber].choices;
+    questionNumber++;
+    for (var q = 0; q < choices.length; q++) {
+        var nextChoice = document.createElement("button");
+
+        nextChoice.textContent = choices[q]
+        answerBtn = choiceContainer.appendChild(nextChoice).setAttribute("class", "btn");
+    }
+}
+
+//function to cause effect/check the interactivity with the click of answers
+function hideFeedback(){
+  var pElement = document.getElementsByClassName("feedback")[0]
+  pElement.style.display='none'
+}
+
+function showFeedback(){
+  var pElement = document.getElementsByClassName("feedback")[0]
+  pElement.style.display= 'block';
+}
+
+choiceContainer.addEventListener("click", function (event) {
+  var pElement = document.getElementsByClassName("feedback")[0]
+  
+  // if/else for answer results
+  if (answer === event.target.textContent) {   
+      pElement.innerHTML = "Correct.";
+      setTimeout(hideFeedback,1225);
+      showFeedback();   
+      
+  } else {
+      pElement.innerHTML = "Incorrect.";
+      setTimeout(hideFeedback,1225);
+      showFeedback();
+  }   
+  getQuestion(); 
+});
+
+function displayScore() {
+  userScore.textContent= timerCount;
+}
+
+function addScore () {
+  userInitials = document.getElementById("initials").value  
+
+  var newScore = {
+      name: userInitials,
+      score: timerCount
+    };
+    // check if there are scores
+    var highScores = JSON.parse(localStorage.getItem("highScores") || "[]");
+    // push object into score array
+    highScores.push(newScore)
+    // turn objects into an array and send to local storage
+    localStorage.setItem("highScores", JSON.stringify(highScores));
+}
+
+function endGame() {
+  document.getElementById("quiz-card").style.display ="none";
+  document.getElementById("end-screen").style.display ="block";
+  displayScore();
+}
+
 startBtn.addEventListener("click", startQuiz);
+submitBtn.addEventListener("click", function (event) {
+  event.stopPropagation();
+  addScore();
+
+  window.location.href= "./highscore.html"
+});
